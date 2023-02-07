c780ee65d52e25964f66a7899c64702673142707
FrancescaBambozzi <bambozzif@gmail.com>
2023-02-07 18:52:00 +0100

    Deploy React app to GitHub Pages

---
+++ precache-manifest.16a4e9bb630d83ee1dd9c11d1e7c9bc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "93f21d9f12e59766822c",
+    "url": "/Sticky-Notes/static/css/main.333dc25d.chunk.css"
+  },
+  {
+    "revision": "93f21d9f12e59766822c",
+    "url": "/Sticky-Notes/static/js/main.606a5e91.chunk.js"
+  },
+  {
+    "revision": "b15224541b42275ca52a",
+    "url": "/Sticky-Notes/static/js/runtime~main.c1a6b2e5.js"
+  },
+  {
+    "revision": "cb46e8a852c2ac0db65c",
+    "url": "/Sticky-Notes/static/js/2.88145d45.chunk.js"
+  },
+  {
+    "revision": "f9ee963bd0d09cbbd5cc854d20417265",
+    "url": "/Sticky-Notes/index.html"
+  }
+];
